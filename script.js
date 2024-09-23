037144184edf80675d5cff45063069b884d72352
king-sovan <156076876+king-sovan@users.noreply.github.com>
2024-09-23 19:20:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-//your JS code here. If required.
+document.getElementById('ageForm').addEventListener('submit', function (event) {
+	event.preventDefault();
+
+	const name = document.getElementById('name').value;
+	const age = document.getElementById('age').value;
+
+	if (!name || !age) {
+		alert('Both fields are required!');
+		return;
+	}
+
+	new Promise((resolve, reject) => {
+		setTimeout(() => {
+			if (parseInt(age) > 18) {
+				resolve();
+			} else {
+				reject();
+			}
+		}, 4000);
+	})
+	.then(() => {
+		alert(`Welcome, ${name}. You can vote.`);
+	})
+	.catch(() => {
+		alert(`Oh sorry, ${name}. You aren't old enough`);
+	});
+});
